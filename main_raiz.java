a7921d03e6936a07c3b125e254c4fae2231bff80
Erick Martinez <erick_martinez9101@outlook.com>
2021-11-22 12:37:54 -0600

    Este es el método de entrada del proyecto Square (raiz cuadrada)

---
+++ main_raiz.java
@@ -0,0 +1,20 @@
+public class Main{
+    public static void main(String[] args){
+
+        //Creando el objeto para mandar a llamar los métodos
+
+        Square root=new Square();
+
+        root.dataInput();
+
+       
+
+
+
+
+
+
+
+
+    }
+}
