c219d667d6301bdfde2fd128223c89e6c728f112
Erick Martinez <erick_martinez9101@outlook.com>
2021-11-23 16:04:05 -0600

    El main del palindromo

---
+++ Main_palindromo.java
@@ -0,0 +1,14 @@
+public class Main_palindromo {
+    public static void main(String[] args) {
+
+        palindromo frase=new palindromo();
+
+        frase.palabras();
+
+        frase.inversa();
+        
+
+
+    }
+    
+}
