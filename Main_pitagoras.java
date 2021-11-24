6ead1f1e8b41dd75ea6f5dc60a3221d526c005d1
Erick Martinez <erick_martinez9101@outlook.com>
2021-11-23 18:13:43 -0600

    Este es el main del teorema de pitagoras

---
+++ Main_pitagoras.java
@@ -0,0 +1,16 @@
+public class Main_pitagoras {
+
+  public static void main(String[] args) {
+
+    pitagoras cateto=new pitagoras();
+
+    cateto.dataInput();
+      
+
+  }
+
+    
+
+    
+    
+}
